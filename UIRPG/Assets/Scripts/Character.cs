9c363386414252286c912f8845b8eb825406b9b9
IIMiKoII <kokainis.mikus@gmail.com>
2025-03-06 12:07:35 +0200

    oooh
    stuff

---
+++ UIRPG/Assets/Scripts/Character.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int health;
+
+    public void Attack()
+    {
+        Debug.Log("Hit");
+    }
+
+}

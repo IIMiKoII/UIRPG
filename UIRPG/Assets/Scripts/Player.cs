fe936af8248ae36b93c77359acf5109de01256bc
IIMiKoII <kokainis.mikus@gmail.com>
2025-06-12 13:56:31 +0300

    Sounds
    -Added attack sound effect
    - Edited player script to play sound effect

--- UIRPG/Assets/Scripts/Player.cs
+++ UIRPG/Assets/Scripts/Player.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class Player : Character
 {
     [SerializeField] private string charName;
+    [SerializeField] private AudioClip attackSFX;
+    [SerializeField] private AudioSource audioSource;
 
     public string CharName
     {
         get { return charName; }
+    }
+
+    public override int Attack()
+    {
+        if (attackSFX != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(attackSFX);
+        }
+
+        return base.Attack();
     }
 }
